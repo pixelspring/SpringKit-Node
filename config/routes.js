4aefa82093b129c367fd22231f293434aab64c8a
Mike Seaby <mike@pixelspring.co.uk>
2016-03-12 15:12:58 +0000

    Update deps

--- config/routes.js
+++ config/routes.js
@@ -36,18 +36,25 @@ module.exports = function (app, passport) {
     /**
      * OAuth authentication routes. (Sign in)
      */
+    // facebook:
     app.get('/auth/facebook', passport.authenticate('facebook', {scope: ['email', 'user_location']}));
     app.get('/auth/facebook/callback', passport.authenticate('facebook', {failureRedirect: '/login'}), function (req, res) {
         res.redirect(req.session.returnTo || '/');
     });
+
+    // GitHub:
     app.get('/auth/github', passport.authenticate('github'));
     app.get('/auth/github/callback', passport.authenticate('github', {failureRedirect: '/login'}), function (req, res) {
         res.redirect(req.session.returnTo || '/');
     });
+
+    // Google
     app.get('/auth/google', passport.authenticate('google', {scope: 'profile email'}));
     app.get('/auth/google/callback', passport.authenticate('google', {failureRedirect: '/login'}), function (req, res) {
         res.redirect(req.session.returnTo || '/');
     });
+
+    // Twitter:
     app.get('/auth/twitter', passport.authenticate('twitter'));
     app.get('/auth/twitter/callback', passport.authenticate('twitter', {failureRedirect: '/login'}), function (req, res) {
         res.redirect(req.session.returnTo || '/');
